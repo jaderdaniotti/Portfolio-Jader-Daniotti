5d5c66f2cddffda288b49ecc986efbb111edde93
jader <jaderdaniotti1999@gmail.com>
2025-06-18 22:18:19 +0200

    inizio sezione about

--- src/pages/chisono.jsx
+++ src/pages/chisono.jsx
@@ -18,7 +18,7 @@ function Chisono() {
                         <div className="list-col-grow flex items-center font-semibold text-xl">
                             <div>Chi sono?</div>
                         </div>
-                        <a href="" className="p-3 hover:scale-120 hover:translate-y-1  transition-all duration-300">
+                        <a href="#aboutme" className="p-3 hover:scale-120 hover:translate-y-1  transition-all duration-300">
                             <i className="bi-eye text-2xl"></i>
                         </a>
                     </li>
@@ -29,7 +29,7 @@ function Chisono() {
                         <div className="list-col-grow flex items-center font-semibold text-xl">
                             <div>La partenza</div>
                         </div>
-                        <a href="" className="p-3 hover:scale-120 hover:translate-y-1  transition-all duration-300">
+                        <a href="#partenza" className="p-3 hover:scale-120 hover:translate-y-1  transition-all duration-300">
                             <i className="bi-eye text-2xl"></i>
                         </a>
                     </li>
@@ -57,6 +57,38 @@ function Chisono() {
                     </li>
 
                 </ul>
+                <hr className="my-10" />
+                <section className="flex flex-col" id="aboutme">
+                    <h2 className="text-center mb-5 text-5xl md:text-6xl ">Chi sono?</h2>
+                    <img src="immagini\AVATAR\10-Photoroom.png" alt="" className="mb-5 h-100 object-contain" />
+                    <p className=" text-3xl  md:text-4xl text-chiaro">
+                        Ciao! Mi chiamo
+                        <span className="titolo-bianco font-extrabold"> Jader </span>
+                        e ti do il benvenuto sul mio sito web.
+                    </p>
+                    <p className=" text-3xl md:text-4xl text-chiaro">
+                        Sono quel tipo di persona a cui piacciono le <span className="titolo-bianco font-extrabold"> sfide </span>, quel tipo di persona a cui piace imparare sempre cose nuove, prendendo spunto da ciò che mi circonda.
+                    </p>
+                    <p className=" text-3xl md:text-4xl text-chiaro">
+                        Se sei arrivato in questa sezione del sito vuol dire che non sei solo interessato al mio lato <span className="titolo-bianco font-extrabold"> professionale </span>, ma di conoscermi veramente, cercherò di farti fare un idea di me!
+                    </p>
+                </section>
+                <hr className="my-10" />
+                <section className="flex flex-col" id="partenza">
+                    <h2 className="text-center mb-5 text-5xl md:text-6xl ">La partenza</h2>
+                    <img src="immagini\AVATAR\9.png" alt="" className="mb-5 h-100 object-contain" />
+                    <p className=" text-3xl  md:text-4xl text-chiaro">
+                        Ho cominciato il mio percorso alle superiori, in un CFP &#40;Corso di Formazione Professionale&#41;. <br />
+                        A quell'età non avevo ancora in mente quello che volevo fare da grande, volevo solo buttarmi sul lavoro il prima possibile e ho fatto 3 anni su 5 in alternanza scuola lavoro. 
+                    </p>
+                    <p className=" text-3xl md:text-4xl text-chiaro">
+                        A 18 anni, stanco della vita che stavo facendo, mi viene offerta un occasione d'oro, quella di partire per un altro stato, in compagnia di un amico.
+                    </p>
+                    <p className=" text-3xl md:text-4xl text-chiaro">
+                        Senza troppo pensarci su, tempo qualche settimana di organizzare i documenti, preparare i vestiti e salutare tutti, mi ritrovo in Francia, più precisamente a Fontaine, periferia di Grenoble, Rhone Alpes.
+                    </p>
+                </section>
+                <hr className="my-10" />
             </div>
             <Footer></Footer>
         </>
