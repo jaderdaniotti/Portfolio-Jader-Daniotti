94a9f80d50286191ff322c7deea2885474aac329
jader <jaderdaniotti1999@gmail.com>
2025-10-09 11:47:38 +0200

    deploy skills e tools

--- src/components/FloatingDashboardButton.jsx
+++ src/components/FloatingDashboardButton.jsx
@@ -25,12 +25,7 @@ const FloatingDashboardButton = () => {
       title={isAdmin ? "Vai al pannello admin" : "Accedi al pannello admin"}
     >
       <LayoutDashboard className="w-6 h-6" />
-      
-      {/* Tooltip */}
-      <div className="absolute bottom-full right-0 mb-2 px-3 py-1 bg-gray-900 text-white text-sm rounded-lg opacity-0 group-hover:opacity-100 transition-opacity duration-300 whitespace-nowrap">
-        
-        <div className="absolute top-full right-4 w-0 h-0 border-l-4 border-r-4 border-t-4 border-transparent border-t-gray-900"></div>
-      </div>
+
     </Link>
   );
 };
